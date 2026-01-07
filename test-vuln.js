da94cb51867581b9d0495d17323102ecc51d78ff
VitorLourenco <40489970+VitorLourenco@users.noreply.github.com>
2026-01-07 21:20:11 +0100

    Create test-vuln.js

---
+++ test-vuln.js
@@ -0,0 +1,13 @@
+  // test-vuln.js
+  const express = require('express');
+
+  // SQL Injection
+  app.get('/user', (req, res) => {
+    const query = `SELECT * FROM users WHERE id = ${req.query.id}`;
+  });
+
+  // Hardcoded Secret (will be detected by Secrets Detection!)
+  const API_KEY = 'sk_live_1234567890abcdef';
+
+  // Command Injection
+  exec(`ls ${req.query.path}`);
